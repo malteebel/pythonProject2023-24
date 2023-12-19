e97fc2419ecc831bbe85eb68fc0e1062390e04e4
Tschupf <lebemalte@gmail.com>
2023-12-19 14:41:16 +0100

    Adjusted comments and variable names

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ path = r'C:\Users\Malte\Desktop\PythonChessBot\pythonProject2023-24\chess_data'
 # get relative paths of all files inside the directory
 pgn_paths = os.listdir(path)
 
-# define empty string variable
+# define empty list
 full_content = []
 
 # iterate through all the files
@@ -19,6 +19,6 @@ for i in range(len(pgn_paths)):
 # join all the content into one string
 merged_string = ''.join(full_content)
 
-
-with open('chess_data.pgn', 'w') as f:
+# writes all combined content to a new file
+with open('game_list.pgn', 'w') as f:
     f.write(merged_string)
