496113a7e69a7ab3dea67af7c9c313e370cdac94
Tschupf <lebemalte@gmail.com>
2023-12-27 23:41:54 +0100

    Relocation of files

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ them into a single file named game_list.pgn for easier preprocessing
 import os
 
 # Define path of all my pgns
-path = r'..\pythonProject2023-24\chess_data'
+path = r'..\pythonProject2023-24\chess_data\downloaded_games'
 
 # Get relative paths of all files inside the directory
 pgn_paths = os.listdir(path)
@@ -25,5 +25,5 @@ for i in range(len(pgn_paths)):
 merged_string = ''.join(full_content)
 
 # Writes all combined content to a new file
-with open('game_list.pgn', 'w') as f:
+with open('chess_data\game_list.pgn', 'w') as f:
     f.write(merged_string)
