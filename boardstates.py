29b312b8be56115f3b76a37600da32663a676e1c
Fplate <fplate@uni-osnabrueck.de>
2024-01-08 21:47:15 +0100

    Commented the boardstate functions

--- boardstates.py
+++ boardstates.py
@@ -1,18 +1,8 @@
 import re
 import numpy as np
-test = dict()
-test["category"] = dict()
-test["category"]["a"] = ["1", "3"]
-test["category"]["b"] = ["2", "4"]
 
 
-
-Legend = dict()
-Legend["N"] = "Knight"
-Legend["Q"] = "Queen"
-Legend["B"] = "Bishop"
-Legend["R"] = "Rook"
-Legend["K"] = "King"
+#create an initial board state
 board_original = dict()
 
 board_original["White"] = dict()
@@ -76,24 +66,25 @@ board_original["Black"]["King"]["King1"] = ["e", "8"]
 board_original["Black"]["Queen"] = dict()
 board_original["Black"]["Queen"]["Queen1"] = ["d", "8"]
 
+#set the state of the game to running
 state = "Running"
 
-print(board_original.keys())
-
-def makeMove(notation, current_board_original = board_original, white = True):
-     #global current_board_original
+#define the make_move function, which changes the board state according to a move notation 
+def make_move(notation, current_board_original = board_original, white = True): 
       global state
+      #create a legend for the different figure encodings
       legend = dict()
       legend["Q"] = "Queen"
       legend["N"] = "Knight"
       legend["B"] = "Bishop"
       legend["R"] = "Rook"
+      #create a list with all currently occupied coordinates
       coordinates = []
       for color in current_board_original.keys():
             for figure in current_board_original[color].keys():
                   for piece in current_board_original[color][figure].values():
                         coordinates.append(piece)
-    #check if black or white are playing, make them play alternately
+      #check if black or white are playing, and assign the variable b(= all fgures of the current player) and bo(= all figures of the other player) accordingly
       if white == True:
             b = current_board_original["White"]
             bo = current_board_original["Black"]
@@ -104,14 +95,19 @@ def makeMove(notation, current_board_original = board_original, white = True):
             bo = current_board_original["White"]
             white = True
             mirror = -1
+      #define a function to find the figure that is supposed to be moved
       def findFigure(keysList, figure):
+            #if the code gives as an N, we must find the correct knight
             if figure == "N":
                   for key in keysList:
+                        #determine the right knight by checking its distance to the goal field
                         if(((abs(ord(notation[-2]) - ord(b["Knight"][key][0])) == 1) and (abs(int(notation[-1]) - int(b["Knight"][key][1])) == 2)) or ((abs(ord(notation[-2]) - ord(b["Knight"][key][0])) == 2) and (abs(int(notation[-1]) - int(b["Knight"][key][1])) == 1))):
                               b["Knight"][key] = [notation[-2], notation[-1]]
                               return current_board_original
+            #find the correct bishop
             elif figure == "B":
                   for key in keysList:
+                        #check if the goal field is on a diagonal to the respective bishop, and if nothing blocks his way
                         if (abs(ord(notation[-2]) - ord(b["Bishop"][key][0])) == abs(int(notation[-1]) - int(b["Bishop"][key][1]))):
                               blocked = False
                               for coordinate in coordinates:
@@ -122,8 +118,10 @@ def makeMove(notation, current_board_original = board_original, white = True):
                               if blocked == False:
                                     b["Bishop"][key] = [notation[-2], notation[-1]]
                                     return current_board_original
+            #find the correct rook
             elif figure == "R":
                   for key in keysList:
+                        #check if the goal field is vertically or horizontally orthogonal to the rook, and whether something blocks the way
                         if notation[-2] in b["Rook"][key] or notation[-1] in b["Rook"][key]:
                               blocked = False
                               for coordinate in coordinates:
@@ -132,9 +130,10 @@ def makeMove(notation, current_board_original = board_original, white = True):
                               if blocked == False:
                                     b["Rook"][key] = [notation[-2], notation[-1]]
                                     return current_board_original
-
+            #find the correct queen
             elif figure == "Q":
                   for key in keysList:
+                        #check the fields diagonal, as well as vertically and horizontally orthogonal to the queen, and check if something blocks the way
                         blocked = False
                         if (abs(ord(notation[-2]) - ord(b["Queen"][key][0])) == abs(int(notation[-1]) - int(b["Queen"][key][1]))):
                               for coordinate in coordinates:
@@ -151,36 +150,44 @@ def makeMove(notation, current_board_original = board_original, white = True):
                         if blocked == False:
                               b["Queen"][key] = [notation[-2], notation[-1]]
                               return current_board_original
+      #remove any additonal signs at the end of the notation
       if notation[-1] in ["#", "+", "!", "?"]:
             notation = notation[:-1]
+      #if we have a pawn promotion, we have to add the figure that the pawn promotes to, and to set it on the right field
       if "=" in notation:
             number = int(list(b[legend[notation[-1]]].keys())[-1][-1])+1
             b[legend[notation[-1]]][legend[notation[-1]]+str(number)] = [notation[-4], notation[-3]]
+      #player lost the game
       if notation == "0-1":
             state = "Lost"
             return current_board_original
+      #player won the game
       if notation == "1-0":
             state = "Win"
             return current_board_original
+      #player has a draw
       if notation == "1/2-1/2":
             state = "Draw"
             return current_board_original
+      #play the short rochade
       if notation == "O-O":
             b["King"]["King1"][0] = "g"
             b["Rook"]["Rook2"][0] = "f"
             print("rochade")
             return current_board_original
+      #play the long rochade
       if notation == "O-O-O":
             b["King"]["King1"][0] = "c"
             b["Rook"]["Rook1"][0] = "d"
             return current_board_original
+      #check if we are beating a figure in this turn
       if "x" in notation:
             beaten = False
             #if that is the case, move the right figure to its goal field and remove the beaten figure from the board_original
             for figure in bo.keys():
                   if beaten == True: break
                   for piece in bo[figure].keys():
-                        #if we have a promotion, the coordinates of the beaten figure are at a different location
+                        #if we have a promotion, the coordinates of the beaten figure are at a different index in the notation
                         if "=" in notation:
                               if bo[figure][piece] == [notation[-4], notation[-3]]:
                                     bo[figure][piece] = ["0", "0"]
@@ -190,6 +197,7 @@ def makeMove(notation, current_board_original = board_original, white = True):
                               bo[figure][piece] = ["0", "0"]
                               beaten = True
                               break
+            #if we have not beaten any figure yet, we probably have an en passé
             if beaten == False:
                   for figure in bo.keys():
                         if beaten == True: break
@@ -199,9 +207,10 @@ def makeMove(notation, current_board_original = board_original, white = True):
                                     bo[figure][piece] = ["0", "0"]
                                     beaten = True
                                     break
+            #if still no figure could be beaten, we give out an error
             if beaten == False:
                   print("Error: No figure could be beaten.")
-
+      #in this case, we simply have to move a pawn
       if len(notation) == 2:
          for key in b["Pawn"].keys():
               #match the file
@@ -263,17 +272,16 @@ def makeMove(notation, current_board_original = board_original, white = True):
                   for key in b[legend[notation[0]]].keys():
                         if notation[1] in b[legend[notation[0]]][key]:
                               keys.append(key)
+                  #after filtering for the figures that fulfill the specification noted in the notation, we use the findFigure function again
                   findFigure(keys, notation[0])
-                  return current_board_original 
+                  return current_board_original
       if len(notation) < 7:
             if "=" in notation:
-                  print("Success1")
+                  #in this case, we have a pawn promotion
                   for key in b["Pawn"].keys():
-                  #match the file
-                        print(b["Pawn"][key][0], notation[0], b["Pawn"][key][1], int(notation[3])-mirror)
+                  #check which pawn would be in reach of the goal field and remove him from the board
                         if b["Pawn"][key][0] == notation[0] and b["Pawn"][key][1] == str((int(notation[3]) - mirror)):
                               b["Pawn"][key] == ["0", "0"]
-                              print("Success!")
                               return current_board_original 
             #in this case, a figure is double-ambigious
             for key in b[legend[notation[0]]].keys():
@@ -284,54 +292,55 @@ def makeMove(notation, current_board_original = board_original, white = True):
       print("error, notation invalid")
       return current_board_original
 
-def getData(path, white=True):
-      mCounter = 0
-      stateList = []
+#function that takes a png file with a game notation and returns the resulting board states as one-hot encoded matrices
+def get_data(path, white=True):
+      #variable that indexes the matrix of figure-types
+      m_counter = 0
+      state_list = []
+      #use the original boardstate before the first move
       global board_original
-      board_original = board_original.copy()
-      if white == True:
-            encoding = 1
-      else:
-            encoding = -1
+      #specify which player is doing the current move
+      encoding = int(white)
       with open(path, 'r') as pgn_file:
             tokens = []
             for line in pgn_file:
-            # Process each line here
+            #find the notation tokens for each line
                   line = line.strip()
                   tokens.append(re.findall(" [^ ]*", line))
+      #for every move notation, perform the move using the make_move function, and append the resulting boardstate to state_list
       for token in tokens:
             for turn in token:
-                  board_originalMatrix = np.zeros((12,8,8))
+                  board_matrix = np.zeros((12,8,8))
                   print(turn[1:])
-                  mCounter = 0
-                  board_original = makeMove(turn[1:], board_original, white)
+                  m_counter = 0
+                  board_original = make_move(turn[1:], board_original, white)
                   for color in board_original.keys():
                         for figure in board_original[color].keys():
                               for piece in board_original[color][figure].values():
                                     if piece[0] != "0":
-                                          board_originalMatrix[mCounter][8 - int(piece[1])][ord(piece[0]) - 97] = 1 * encoding
-                              mCounter += 1
+                                          board_matrix[m_counter][8 - int(piece[1])][ord(piece[0]) - 97] = 1 * encoding
+                              m_counter += 1
                         encoding = encoding * -1
-                  stateList.append(board_originalMatrix)
+                  state_list.append(board_matrix)
                   white = (white == False)
-      return(stateList)
+      return(state_list)
       
 
 #test
-"""makeMove("h4")
-makeMove("e5")
-makeMove("a4")
-makeMove("b5")
-makeMove("Rh3")
-makeMove("a5")
-makeMove("Raa3")
-makeMove("c5")
-makeMove("axb5")
-makeMove("d5")
-makeMove("Rxa5")
-makeMove("Bb7")
+"""make_move("h4")
+make_move("e5")
+make_move("a4")
+make_move("b5")
+make_move("Rh3")
+make_move("a5")
+make_move("Raa3")
+make_move("c5")
+make_move("axb5")
+make_move("d5")
+make_move("Rxa5")
+make_move("Bb7")
 """
 #print(board_original.items())
 
-getData("/Users/franziska-marieplate/Documents/5. Semester/Python/Chess/pythonProject2023-24/chess_data/all_games/game_1.pgn")
+get_data("/Users/franziska-marieplate/Documents/5. Semester/Python/Chess/pythonProject2023-24/chess_data/all_games/game_1.pgn")
 #getData("/chess_data/all_games/game_1.pgn")
