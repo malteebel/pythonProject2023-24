10309c54d39397331ac53ca7826a41c9357e5db8
Tschupf <lebemalte@gmail.com>
2024-01-08 17:09:53 +0100

    Deleted unnecessary files and renamed others

--- training.py
+++ training.py
@@ -1,10 +1,73 @@
+"""
+This script contains both training functions one using the .fit() and
+.compile() method of the high level tensorflow library. The manual 
+loop does not use this method to allow for a more precise training 
+approach.
+
+THE MANUAL LOOP IS CURRENTLY NOT WORKING.
+"""
+
 import tensorflow as tf
-from keras.callbacks import EarlyStopping
-import tqdm
+from keras.callbacks import EarlyStopping, TensorBoard
 import datetime
+import tqdm
 from model import ChessANN
 
+def training_loop_fit(train_ds, test_ds, epochs, config_name):
+    """
+    This function compiles the model with set loss function and 
+    optimizer and fits is to the fed data.
+
+    Args:
+        train_ds (tf.data.Dataset): training dataset
+        test_ds (tf.data.Dataset): test dataset
+        epochs (int): number of epochs training
+        config_name (string): Name of the training configuration
+    """
+
+    # Log paths for tensorboard
+    log_path = f"logs/fit/{config_name}"
+
+    # Initialize model
+    model = ChessANN()
+
+    # Add early stopping to save training time if model is not 
+    # learning
+    early_stopping = EarlyStopping(
+        monitor='val_test_loss', patience=10, 
+        restore_best_weights=True)
+    
+    # Add tensorboard to help visualize loss, open tensorboard before
+    # training using: tensorboard --logdir logs/fit
+    tensorboard_callback = TensorBoard(
+        log_dir=log_path, histogram_freq=3)
+    
+    # Compile model with set metrics 
+    model.compile(optimizer="Adam", loss="MSE", metrics=['loss', 'accuracy'])
+
+    # Fit model directly to data with early stopping callback
+    model.fit(
+        train_ds,
+        epochs=epochs,
+        validation_data=test_ds,
+        callbacks=[early_stopping, tensorboard_callback])
+    
+    # Save the weights of the model (does not save optimizer state 
+    # and is therefore not optimal for loading and continiuing 
+    # training), used for predictions
+    model.save_weights(f"models/saved_model_{config_name}", save_format="tf")
+
+
 def training_loop_man(train_ds, test_ds, epochs, config_name):
+    """
+    UNFINISHED AND NOT WORKING
+
+    Args:
+        train_ds (_type_): _description_
+        test_ds (_type_): _description_
+        epochs (_type_): _description_
+        config_name (_type_): _description_
+    """
 
     # Time for logging
     time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -63,7 +126,3 @@ def training_loop_man(train_ds, test_ds, epochs, config_name):
 
     # Saves model
     # model.save(f"models\EPOCHS{epochs}_{time}", save_format="tf")
-
-
-
-
